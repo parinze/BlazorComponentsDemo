eae949164bfb82d6f33af5b0a5479046eb95aae1
Priscilla Arinze <PArinze@pattersoncompanies.com>
2024-02-22 17:44:17 -0600

    -Added larger test dataset -Removed highlighting styles from main RazdenDataGrid -Moved highlighting style to test RazenDataGrid -Added some test JS interop stuff (commented out for now)

---
+++ BlazorComponentsDemo.ComponentsLibrary/Models/TestDataLarge.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorComponentsDemo.ComponentsLibrary.Models
+{
+	public class TestDataLarge
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public DateTime BirthDate { get; set; }
+		public bool Status { get; set; }
+    }
+}
---
+++ BlazorComponentsDemo/wwwroot/scripts/radzendatagridscript.js
@@ -0,0 +1,16 @@
+﻿// By default, RadzenDataGrid will initially display '10' for the page size dropdown label on page load.
+// This function sets the label to the correct minimum page size number.
+export function changeDropdownLabelText(minPageSize) {
+    // Get the label element with both classes "rz-dropdown-label" and "rz-inputtext"
+    var label = document.querySelector('.rz-dropdown-label.rz-inputtext');
+
+    if (label) {
+        label.textContent = minPageSize;
+    } else {
+        return;
+    }
+}
+
+export function showAlert(message) {
+    alert(message);
+}
