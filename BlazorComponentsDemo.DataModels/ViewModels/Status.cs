82554df22f8e8189ce14a7091aa8a659b6e48b67
Priscilla Arinze <PArinze@pattersoncompanies.com>
2024-02-27 10:15:09 -0600

    -Added Status view model and updated Status column logic -Added 2nd version of main RadzenDataGrid

---
+++ BlazorComponentsDemo.DataModels/ViewModels/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorComponentsDemo.DataModels.ViewModels
+{
+    public class Status {
+	public string Label { get; set; }
+    public string Style { get; set; }
+    public byte? Code { get; set; }
+}
+}
