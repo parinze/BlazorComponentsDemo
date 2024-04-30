0188b05d4ce3567945f698768eef6e09827ffbc1
Priscilla Arinze <PArinze@pattersoncompanies.com>
2024-04-30 17:22:59 -0500

    added layout chooser component (treatment grid)

---
+++ BlazorComponentsDemo.DataModels/Models/LayoutDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorComponentsDemo.DataModels.Models
+{
+    public class LayoutDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? Index { get; set; }
+        public string TabColor { get; set; }
+        public string ForeColor { get; set; }
+        public int? OleColor { get; set; }
+    }
+}
